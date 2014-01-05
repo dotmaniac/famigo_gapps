ee8f9e10d59c6f005d8772c781e9275a59b51d03
Brandon Giesing <dotmaniac39@gmail.com>
2014-01-05 16:00:51 -0500

    Revert "Addon.d Fixes" and fix it the correct way
    This reverts commit 4ec2d8870eb4b215c8583b4ae65eb121de873856.
    
    Conflicts:
    	META-INF/com/google/android/updater-script

---
+++ addon.d/gapps/80-hangouts.sh
@@ -0,0 +1,25 @@
+#!/sbin/sh
+
+. /tmp/backuptool.functions
+
+case "$1" in
+  backup)
+    # Stub
+  ;;
+  restore)
+	rm -f /system/app/Mms.apk
+	rm -f /system/priv-app/Mms.apk
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    # Stub
+;;
+esac
